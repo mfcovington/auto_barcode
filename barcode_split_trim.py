f0ecad3a21000c773ff57201d761f1331e843012
Mike Covington <mfcovington@gmail.com>
2014-02-23 15:32:03 -0800

    Add pseudocode outline for main function

---
+++ barcode_split_trim.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def main():
+    # validate_options()
+    # barcode_table = get_barcodes()
+    # barcode_length = validate_barcodes(barcode_table)
+    # open_fq_fhs_in_bulk()
+    # split_trim_barcodes()
+    # close_fq_fhs()
+    # summarize_observed_barcodes()
+    # summarize_counts()
+    # plot_summary()
+    pass
+
+if __name__ == '__main__':
+    main()
